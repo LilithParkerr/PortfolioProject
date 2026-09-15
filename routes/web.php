b2c22173910198964d18d7a2dfd194ad6c8ef0f8
LilithParkerr <drp0016@edu.curio.nl>
2026-09-15 10:15:55 +0200

    admin route changed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,9 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin', function () {
-        return 'Welcome to the admin area!';
-    })->name('admin.dashboard');
-});
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::resource('projects', ProjectController::class);
+    });
 require __DIR__.'/auth.php';
